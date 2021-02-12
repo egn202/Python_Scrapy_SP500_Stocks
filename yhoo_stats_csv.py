388ffa9c962f00d3bf6798937e19e7674305658a
Eugene Ng <egn202@gmail.com>
2021-02-12 15:31:17 -0500

    updated selenium script

--- yhoo_stats_csv.py
+++ yhoo_stats_csv.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 # import re
-import time
+# import time
 
 # Windows users need to specify the path to chrome driver you just downloaded.
 # You need to unzip the zipfile first and move the .exe file to any folder you want.
@@ -20,7 +20,7 @@ writer = csv.writer(csv_file)
 
 urls = [f'https://finance.yahoo.com/quote/{ticker}/key-statistics?p={ticker}' for ticker in tickers]
 
-writer.writerow(['ticker','px2sales'])
+writer.writerow(['ticker','ev','trail_pe','fwd_pe','peg','px2sales','px2book','ev2rev','ev2ebitda'])
 
 for url in urls:
     stats_dict = {}
@@ -29,17 +29,27 @@ for url in urls:
     # time.sleep(2)
 
     tk = driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1').text
+    ev = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[2]/td[2]').text
+    trail_pe = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[3]/td[2]').text
+    fwd_pe = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[4]/td[2]').text
+    peg = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[5]/td[2]').text
     px2sales = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[6]/td[2]').text
+    px2book = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[7]/td[2]').text
+    ev2rev = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[8]/td[2]').text
+    ev2ebitda = driver.find_element_by_xpath('//*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[9]/td[2]').text
 
     stats_dict['tk'] = tk
+    stats_dict['ev'] = ev
+    stats_dict['trail_pe'] = trail_pe
+    stats_dict['fwd_pe'] = fwd_pe
+    stats_dict['peg'] = peg
     stats_dict['px2sales'] = px2sales
+    stats_dict['px2book'] = px2book
+    stats_dict['ev2rev'] = ev2rev
+    stats_dict['ev2ebitda'] = ev2ebitda
 
     writer.writerow(stats_dict.values())
 
 csv_file.close()
 driver.close()
 
-
-# //*[@id="Col1-0-KeyStatistics-Proxy"]/section/div[3]/div[1]/div[2]/div/div[1]/div[1]/table/tbody/tr[6]/td[2]
-# //*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1
-# //*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1
